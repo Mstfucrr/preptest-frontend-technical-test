02a4118f79001a96762c94ac1969267b7c620957
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2024-01-05 17:51:28 +0300

    Add Home page component

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideBar from 'src/components/sideBar'
+
+const Home = () => {
+    return (
+        <div className="w-3/5 mx-auto">
+            <div className="flex">
+                <SideBar />
+            </div>
+        </div>
+    )
+}
+
+export default Home
