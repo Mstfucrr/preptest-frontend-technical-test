3088e3b87ea6b4bb1f597feed7e8e83d14b03a62
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2024-01-06 00:47:01 +0300

    Refactor home page component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,11 @@
-import React from 'react'
+import { AnimatePresence } from 'framer-motion'
+import React, { useState } from 'react'
+import HomeComponent from 'src/components/home'
 import SideBar from 'src/components/sideBar'
 
 const Home = () => {
     return (
-        <div className="w-3/5 mx-auto">
-            <div className="flex">
-                <SideBar />
-            </div>
-        </div>
+        <HomeComponent />
     )
 }
 
