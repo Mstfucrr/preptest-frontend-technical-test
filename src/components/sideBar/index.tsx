f2e524931f649519828ee7b39dd7bc6f6719ac61
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2024-01-06 03:29:27 +0300

    update the sidebar to use it on other pages

--- src/components/sideBar/index.tsx
+++ src/components/sideBar/index.tsx
@@ -1,105 +1,149 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
-import { motion } from 'framer-motion';
-import { PagesElement } from '../home';
+import { AnimatePresence, motion } from 'framer-motion';
+import { classesIcon, examIcon, staticsIcon, recommandationIcon, calendarIcon } from './icons';
+import { PagesElement } from 'src/types/types';
 
 //logo from public
+const SideBar = () => {
 
-type props = {
-    isSideBarOpen: boolean,
-    pagesElement: PagesElement[],
-    activePage: PagesElement,
-    setActivePage: React.Dispatch<React.SetStateAction<PagesElement>>
+    const [isSideBarOpen, setIsSideBarOpen] = useState(true)
 
-}
+    const pagesElement = [
+        {
+            title: 'Classes',
+            Icon: classesIcon,
+        },
+        {
+            title: 'Exam Module',
+            Icon: examIcon,
+        },
+        {
+            title: 'Statics',
+            Icon: staticsIcon,
+        },
+        {
+            title: 'Recommandation',
+            Icon: recommandationIcon,
+        },
+        {
+            title: 'Calendar',
+            Icon: calendarIcon,
+        }
+    ] as PagesElement[];
 
-const SideBar = ({ isSideBarOpen, pagesElement, activePage, setActivePage }: props) => {
+    const [activePage, setActivePage] = useState<PagesElement>(pagesElement[0])
+
 
     // classes, exam module, Statics, Recommandation, Calendar
     return (
-        <motion.div className="w-64 h-full bg-customColors-lightBg pl-8 relative border-[80_82_84_0.25] border border-solid"
-            animate={{
-                width: isSideBarOpen ? 270 : 0,
-                scaleX: isSideBarOpen ? 1 : 0,
-                opacity: isSideBarOpen ? 1 : 0,
-                originX: 0,
-                transition: {
-                    duration: 0.5,
-
-                },
-
-            }}
-
-        >
-
-            {/* logo */}
-            <div className="flex flex-col justify-start">
-
-                <div className="flex mt-16 ">
-                    <Image src={'/logo.svg'} className='w-full' width={200} height={30} />
-                </div>
-
-                {/* dashboard */}
-                <div className="flex flex-row gap-3 mt-24 ">
-                    <Image src={'/icons/dashboard.svg'} width={24} height={24} />
-                    <p className="text-md font-medium text-customColors-darkTxt">Dashboard</p>
-                </div>
-
-                {/* pages */}
-
-                {/* pages Title */}
-                <div className="mt-9">
-                    <p className="font-medium text-[#262626] uppercase poppins">Pages</p>
-                </div>
-
-                {/* pages element */}
-                <div className="flex flex-col gap-7 mt-9">
-
-                    {pagesElement.map((Element) => (
-                        <button key={"page-" + Element.title} className="flex flex-row gap-3  text-primary-600"
-                            onClick={() =>
-                                setActivePage(Element)}
-                        >
-                            <Element.Icon fill={activePage.title === Element.title ? '#ec5523' : '#000000DE'} />
-
-                            {/* 0, 0, 0, 0.87 */}
-                            <p className={`text-md font-normal inter ${activePage.title === Element.title ? 'text-primary-600' : 'text-[#262626]'}`}>{Element.title}</p>
-                        </button>
-                    ))}
-                </div>
 
 
-                {/* ACCOUNT SETTINGS */}
-                <div className="mt-28">
-                    <p className="font-medium text-[#4C4E6480] uppercase poppins">Account Settings</p>
-                </div>
-                <div className="flex flex-col gap-y-7 mt-4">
+        <AnimatePresence>
 
-                    {/* Message Board */}
-                    <div className="flex flex-row gap-3 ">
-                        <Image src={'/icons/message.svg'} width={24} height={24} />
-                        <p className="text-sm font-normal poppins text-[#000000DE]">Message Board</p>
+            <motion.div className="w-64 h-full bg-customColors-lightBg pl-8 relative border-[80_82_84_0.25] border border-solid"
+                animate={{
+                    width: isSideBarOpen ? 270 : 0,
+                    scaleX: isSideBarOpen ? 1 : 0,
+                    opacity: isSideBarOpen ? 1 : 0,
+                    originX: 0,
+                    transition: {
+                        duration: 0.5,
+
+                    },
+
+                }}
+
+            >
+
+                {/* logo */}
+                <div className="flex flex-col justify-start">
+
+                    <div className="flex mt-16 ">
+                        <Image src={'/logo.svg'} className='w-full' width={200} height={30} />
                     </div>
-                    {/* Settings */}
-                    <div className="flex flex-row gap-3">
-                        <Image src={'/icons/settings.svg'} width={24} height={24} />
-                        <p className="text-sm font-normal poppins text-[#000000DE]">Settings</p>
+
+                    {/* dashboard */}
+                    <div className="flex flex-row gap-3 mt-24 ">
+                        <Image src={'/icons/dashboard.svg'} width={24} height={24} />
+                        <p className="text-md font-medium text-customColors-darkTxt">Dashboard</p>
                     </div>
-                    {/* Logout */}
-                    <div className="flex flex-row gap-3">
-                        <Image src={'/icons/logout.svg'} width={24} height={24} />
-                        <p className="text-sm font-normal poppins text-[#000000DE]">Logout</p>
+
+                    {/* pages */}
+
+                    {/* pages Title */}
+                    <div className="mt-9">
+                        <p className="font-medium text-[#262626] uppercase poppins">Pages</p>
                     </div>
-                    {/* Terms & Conditions */}
-                    <div className="flex flex-row gap-3">
-                        <Image src={'/icons/terms.svg'} width={24} height={24} />
-                        <p className="text-sm font-normal poppins text-[#000000DE]">Terms & Conditions</p>
+
+                    {/* pages element */}
+                    <div className="flex flex-col gap-7 mt-9">
+
+                        {pagesElement.map((Element) => (
+                            <button key={"page-" + Element.title} className="flex flex-row gap-3  text-primary-600"
+                                onClick={() =>
+                                    setActivePage(Element)}
+                            >
+                                <Element.Icon fill={activePage.title === Element.title ? '#ec5523' : '#000000DE'} />
+
+                                {/* 0, 0, 0, 0.87 */}
+                                <p className={`text-md font-normal inter ${activePage.title === Element.title ? 'text-primary-600' : 'text-[#262626]'}`}>{Element.title}</p>
+                            </button>
+                        ))}
                     </div>
+
+
+                    {/* ACCOUNT SETTINGS */}
+                    <div className="mt-28">
+                        <p className="font-medium text-[#4C4E6480] uppercase poppins">Account Settings</p>
+                    </div>
+                    <div className="flex flex-col gap-y-7 mt-4">
+
+                        {/* Message Board */}
+                        <div className="flex flex-row gap-3 ">
+                            <Image src={'/icons/message.svg'} width={24} height={24} />
+                            <p className="text-sm font-normal poppins text-[#000000DE]">Message Board</p>
+                        </div>
+                        {/* Settings */}
+                        <div className="flex flex-row gap-3">
+                            <Image src={'/icons/settings.svg'} width={24} height={24} />
+                            <p className="text-sm font-normal poppins text-[#000000DE]">Settings</p>
+                        </div>
+                        {/* Logout */}
+                        <div className="flex flex-row gap-3">
+                            <Image src={'/icons/logout.svg'} width={24} height={24} />
+                            <p className="text-sm font-normal poppins text-[#000000DE]">Logout</p>
+                        </div>
+                        {/* Terms & Conditions */}
+                        <div className="flex flex-row gap-3">
+                            <Image src={'/icons/terms.svg'} width={24} height={24} />
+                            <p className="text-sm font-normal poppins text-[#000000DE]">Terms & Conditions</p>
+                        </div>
+                    </div>
+
                 </div>
 
-            </div>
+            </motion.div>
+            {/* sidebar close icon */}
+            <button className={`flex justify-center items-center w-12 h-12 bg-customColors-lightBg rounded-full absolute cursor-pointer`}
+                onClick={() => setIsSideBarOpen(!isSideBarOpen)}
+                style={{
+                    transform: isSideBarOpen ? 'rotate(0deg)' : 'rotate(180deg)',
+                    top: '65px',
+                    transformOrigin: 'center',
+                    left: isSideBarOpen ? '90%' : '1%',
+                    transition: 'all 0.55s '
+                }}
+            >
+                <div className="text-customColors-lightText text-2xl font-bold">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-customColors-lightText" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                            d="M15 19l-7-7 7-7" />
+                    </svg>
+                </div>
 
-        </motion.div>
+            </button>
+        </AnimatePresence>
     )
 }
 
