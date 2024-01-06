e29d59cba3929fdea4b9cfb5e33872a660641ed5
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2024-01-06 03:28:14 +0300

    Refactor code to improve performance and readability

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,57 +1,18 @@
-import { AnimatePresence, motion } from 'framer-motion'
-import React, { useState } from 'react'
+import { motion } from 'framer-motion'
+import React from 'react'
 import Image from 'next/image'
 import SideBar from '../sideBar'
-import { classesIcon, examIcon, staticsIcon, recommandationIcon, calendarIcon } from '../sideBar/icons'
 import TopBar from '../topBar'
 import StatisticCard from '../statisticCard'
 import TestCardComponent from '../testCard'
+import TestTitle from '../testTitle'
+import dynamic from 'next/dynamic';
+import { TestCard } from 'src/types/types'
 
-export interface PagesElement {
-    title: string;
-    Icon: React.FC<{ fill: string }>; // Update the type to accept an `fill` prop
-}
 
-export interface TestCard {
-    id: number;
-    title: string;
-    testCount: number;
-    questionCount: number;
-    subtopicCount: number;
-    time: number;
-    difficulty: number;
-    isDone: boolean;
-    isLocked: boolean;
-}
+
 
 const HomeComponent = () => {
-    const [isSideBarOpen, setIsSideBarOpen] = useState(true)
-
-    const pagesElement = [
-        {
-            title: 'Classes',
-            Icon: classesIcon,
-        },
-        {
-            title: 'Exam Module',
-            Icon: examIcon,
-        },
-        {
-            title: 'Statics',
-            Icon: staticsIcon,
-        },
-        {
-            title: 'Recommandation',
-            Icon: recommandationIcon,
-        },
-        {
-            title: 'Calendar',
-            Icon: calendarIcon,
-        }
-    ] as PagesElement[];
-
-    const [activePage, setActivePage] = useState<PagesElement>(pagesElement[0])
-
 
     const testCards: TestCard[] = [
         {
@@ -78,53 +39,81 @@ const HomeComponent = () => {
         }
     ]
 
+    const lockedTestCards: TestCard[] = [
+        {
+            id: 1,
+            title: 'Data Analysis-1',
+            testCount: 1,
+            subtopicCount: 10,
+            questionCount: 55,
+            time: 60,
+            difficulty: 25,
+            isDone: false,
+            isLocked: true
+        },
+        {
+            id: 2,
+            title: 'Data Analysis-2',
+            testCount: 2,
+            subtopicCount: 10,
+            questionCount: 55,
+            time: 60,
+            difficulty: 25,
+            isDone: false,
+            isLocked: true
+        }
+    ]
 
+    const MotionLink = dynamic(() => import('framer-motion').then((mod) => mod.motion.a), { ssr: false });
 
 
     return (
-        <div className="lg:w-4/5 xl:w-3/5 w-full mx-auto flex lg:flex-row items-start">
-            <div className="flex relative w-auto h-screen">
+        <div className="lg:w-4/5 xl:w-3/5 w-full mx-auto flex lg:flex-row items-start h-screen">
+            <div className="flex relative w-auto min-h-screen">
                 <div className="lg:flex hidden">
-
-                    <AnimatePresence>
-                        <SideBar isSideBarOpen={isSideBarOpen} pagesElement={pagesElement} activePage={activePage} setActivePage={setActivePage} />
-
-                        {/* sidebar close icon */}
-                        <div className="">
-                            {/* < or > */}
-                            <button className={`flex justify-center items-center w-12 h-12 bg-customColors-lightBg rounded-full absolute cursor-pointer`}
-                                onClick={() => setIsSideBarOpen(!isSideBarOpen)}
-                                style={{
-                                    transform: isSideBarOpen ? 'rotate(0deg)' : 'rotate(180deg)',
-                                    top: '65px',
-                                    transformOrigin: 'center',
-                                    left: isSideBarOpen ? '90%' : '1%',
-                                    transition: 'all 0.55s '
-                                }}
-                            >
-                                <div className="text-customColors-lightText text-2xl font-bold">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-customColors-lightText" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                            d="M15 19l-7-7 7-7" />
-                                    </svg>
-                                </div>
-
-                            </button>
-                        </div>
-                    </AnimatePresence>
+                    <SideBar />
                 </div>
             </div>
             {/* main content */}
-            <div className="flex w-full flex-col gap-7 px-9 my-5">
+            <div className="flex w-full flex-col px-9 mb-5 max-h-screen overflow-y-auto">
 
                 {/* topbar */}
                 <TopBar />
+                <div className="flex w-full">
+                    <div className="flex w-full h-16 bg-customColors-orangeBg rounded-2xl text-orangeText items-center justify-between">
+                        <motion.button className="flex ml-4 items-center justify-center"
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 1 }}
+                            transition={{ duration: 0.2 }}
+                        >
+                            <Image src={'/icons/arrow-left.svg'} width={16} height={16} />
+                            <p className="text-lg font-semibold ml-3 poppins">
+                                Fonksionlar
+                            </p>
+                        </motion.button>
+                        <div>
+                            <p className="text-2xl font-semibold leading-normal mr-4 poppins uppercase"> Test </p>
+                        </div>
+                        <motion.button className="flex mr-4 items-center justify-center"
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 1 }}
+                            transition={{ duration: 0.2 }}
+                        >
+                            <p className="text-lg font-semibold mr-3 poppins">
+                                Polinomlar
+                            </p>
+                            <Image src={'/icons/arrow-right.svg'} width={16} height={16} />
+                        </motion.button>
+                    </div>
+                </div>
                 {/* content */}
-                <div className="flex flex-row flex-wrap gap-y-4">
+                <div className="flex flex-row flex-wrap gap-y-4 mt-7">
                     {/* notes card */}
-                    <motion.button className='flex w-full xl:w-2/3 sm:min-w-[35rem] max-h-72 flex-col justify-around items-center  p-6 
+                    <MotionLink className='flex w-full xl:w-2/3 sm:min-w-[35rem] max-h-72 flex-col justify-around items-center p-6 
                     rounded-xl bg-gradient-to-b from-customColors-orangeBg via-transparent to-[rgba(255,123,93,0.40)]
-                    hover:shadow-lg transition duration-300 ease-in-out relative '>
+                    hover:shadow-lg transition duration-300 ease-in-out relative '
+                        href='/notes'
+                    >
 
                         {/* title */}
                         <p className="text-orangeText uppercase text-2xl font-medium">Notes: Data Analysis</p>
@@ -137,9 +126,7 @@ const HomeComponent = () => {
                                 </div>
                             </div>
 
-                            {/* yazı aşağıya doğrı blurlu olsun */}
-                            <p className="text-left text-[#262626] text-lg font-normal poppins
-                            w-full h-full overflow-hidden">
+                            <p className="text-left text-[#262626] text-lg font-normal poppins w-full h-full overflow-hidden">
 
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, voluptas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, voluptas.
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -150,42 +137,58 @@ const HomeComponent = () => {
                         {/* read more */}
                         <div className="absolute w-full h-14 bottom-0 bg-customColors-orangeBg blur-2xl" />
                         {/* button */}
-                        <motion.button
-                            className='flex justify-center items-center gap-3 py-2 px-6 mb-4 bg-orangeText rounded-full absolute z-10 bottom-0'
+
+
+                        <motion.a
+                            className='flex justify-center items-center gap-3 py-2 px-6 mb-4 bg-orangeText rounded-full absolute bottom-0'
                             whileHover={{ scale: 1.1 }}
                             whileTap={{ scale: 1 }}
-                        >
+                            href='/notes'>
                             <p className="text-customColors-lightBg font-medium">Read More</p>
                             <Image src="/icons/arrow-bottom.svg" width={17} height={17} />
 
-                        </motion.button>
-
-
-                    </motion.button>
+                        </motion.a>
+                    </MotionLink>
 
                     {/* statistic card */}
                     <StatisticCard />
 
-                    {/* NON-Generated Test */}
-                    <div className="flex w-full h-[90px] p-6 py-10 bg-customColors-orangeBg rounded-2xl text-orangeText items-center relative justify-between z-10">
-                        <div className="flex flex-col z-10">
-                            <p className="text-xl font-semibold text-[#505254] px-6 poppins">NON-Generated Test</p>
-                            <p className="text-sm font-normal text-[#505254BF] px-6 poppins">These are tests specially prepared by our teachers.</p>
-                        </div>
-                        <div className="blur-sm absolute top-1 -right-6">
-                            <img src="/images/non.png"
-                                alt="non"
-                                className="w-[230px] object-cover max-h-20 object-top -z-10" />
-                        </div>
-
-                    </div>
-
+                    {/* Test Title */}
+                    <TestTitle title="NON-Generated Test" description="These are tests specially prepared by our teachers." isNonGeneretad={true} />
                     {/* Data Analysis cars 1-2 */}
                     <div className="w-full grid grid-rows-1 lg:grid-cols-2 grid-cols-1 gap-7">
                         {testCards.map((testCard) => (
                             <TestCardComponent testCard={testCard} key={"testcard-" + testCard.id} />
                         ))}
                     </div>
+
+                    {/* Test Title */}
+                    <TestTitle title="Generated Test" description="These are tests specially prepared by our teachers." isNonGeneretad={false} />
+                    {/* Data Analysis cars 1-2 */}
+                    <div className="w-full grid grid-rows-1 lg:grid-cols-2 grid-cols-1 gap-7">
+                        {lockedTestCards.map((testCard) => (
+                            <TestCardComponent testCard={testCard} key={"testcard-" + testCard.id} />
+                        ))}
+                    </div>
+
+                    {/* Test Title */}
+                    <TestTitle title="NON-Generated Test" description="These are tests specially prepared by our teachers." isNonGeneretad={true} />
+                    {/* Data Analysis cars 1-2 */}
+                    <div className="w-full grid grid-rows-1 lg:grid-cols-2 grid-cols-1 gap-7">
+                        {testCards.map((testCard) => (
+                            <TestCardComponent testCard={testCard} key={"testcard-" + testCard.id} />
+                        ))}
+                    </div>
+
+                    {/* Test Title */}
+                    <TestTitle title="Generated Test" description="These are tests specially prepared by our teachers." isNonGeneretad={false} />
+                    {/* Data Analysis cars 1-2 */}
+                    <div className="w-full grid grid-rows-1 lg:grid-cols-2 grid-cols-1 gap-7">
+                        {lockedTestCards.map((testCard) => (
+                            <TestCardComponent testCard={testCard} key={"testcard-" + testCard.id} />
+                        ))}
+                    </div>
+
                 </div>
             </div >
         </div >
